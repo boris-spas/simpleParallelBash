6fd258de40f98f046df121a88fddc0a1de01a848
unregistered88 <boris.spas@yahoo.com>
2014-03-20 14:06:59 +0100

    things happen in per thread folders

--- thread.sh
+++ thread.sh
@@ -3,18 +3,35 @@
 
 TMP_OS=`uname | tr "[:upper:]" "[:lower:]"`
 if [[ "{$TMP_OS}" = *darwin* ]]; then
-    LOCK='lockfile lock.lock'
-    UNLOCK='rm -f lock.lock'
+    LOCK='lockfile ../lock.lock'
+    UNLOCK='rm -f ../lock.lock'
 elif [[ "{$TMP_OS}" = *linux* ]]; then
-    LOCK='lockfile-create lock'
-    UNLOCK='lockfile-remove lock'
+    LOCK='lockfile-create ../lock'
+    UNLOCK='lockfile-remove ../lock'
 fi
 
 ID=$1
-EXEC=$2
-DATA_FILE=$3
+if [[ "$2" = /* ]]
+then
+   : # Absolute path
+   EXEC=$2
+else
+   : # Relative path
+   EXEC=`pwd`/$2
+fi
+
+if [[ "$3" = /* ]]
+then
+   : # Absolute path
+   DATA_FILE=$3
+else
+   : # Relative path
+   DATA_FILE=`pwd`/$3
+fi
 LIMIT=`awk 'END{print NR}' $DATA_FILE`
-COUNTER='syncedCount.txt'
+COUNTER='../syncedCount.txt'
+mkdir $1
+cd $1
 while :
 do
   $LOCK
@@ -24,14 +41,17 @@ do
   echo "Thread $ID sais: new index value: "$index
   echo $index > $COUNTER
   $UNLOCK
-  if [ "$index" -lt "$LIMIT" ]; then
+  if [ "$index" -le "$LIMIT" ]; then
 	WORK_LOAD=`sed -n \`echo $index\`p < $DATA_FILE`
 	echo "Thread $ID sais: working on "$WORK_LOAD
         echo ""
-	$EXEC $WORK_LOAD > "standardOutput/$WORK_LOAD.txt"
+	$EXEC $WORK_LOAD > "../standardOutput/$WORK_LOAD.txt"
   else
 	echo "Thread $ID done, no more work"
 	echo ""
+	cd ..
+	rm -rf $1
 	exit
   fi
 done
+
